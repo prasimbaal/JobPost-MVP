225fee9081f52259b50f0dfd9b3d6db836aaeefc
prasimbaal <prasimbaal@gmail.com>
2026-03-05 10:35:22 -0500

    footer added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,26 @@
 <template>
-    <div :class="user.darkMode ? 'dark' : ''">
-      <div class="min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
-        <Navbar />
-        <div class="px-6 py-6">
-          <slot />
-        </div>
-      </div>
+  <div :class="user.darkMode ? 'dark' : ''">
+    <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+
+      <!-- Navbar -->
+      <Navbar />
+
+      <!-- Main content -->
+      <main class="flex-1 px-6 py-6">
+        <slot />
+      </main>
+
+      <!-- Footer -->
+      <Footer />
+      
     </div>
-  </template>
-  
-  <script setup>
-  import Navbar from '../components/layout/Navbar.vue'
-  import Footer from '../components/layout/Footer.vue'
-  import { useUserStore } from '../stores/userStore'
+  </div>
+</template>
 
+<script setup>
+import Navbar from '../components/layout/Navbar.vue'
+import Footer from '../components/layout/Footer.vue'
+import { useUserStore } from '../stores/userStore'
 
-  const user = useUserStore()
-
+const user = useUserStore()
 </script>
